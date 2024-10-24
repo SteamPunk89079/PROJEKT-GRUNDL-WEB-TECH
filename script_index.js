9d6ad5cd43088d08700f010b4c020184d15cf237
SteamPunk89079 <99643838+SteamPunk89079@users.noreply.github.com>
2024-10-24 22:43:47 +0200

    Second commit, better structure

---
+++ script_index.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+const karte_button = document.getElementById("karte_button");
+const musik_angebot_page = document.getElementById("karte_button");
+const abstimmung = document.getElementById("abstimmung_page");
+
+
+karte_button.addEventListener('click', function() {
+
+    window.location.href = "Pages/karte_page";
+});
+
+musik_angebot_button.addEventListener('click', function() {
+
+    window.location.href = "Pages/musik_angebot_page";
+});
+
+abstimmung_button.addEventListener('click', function() {
+
+    window.location.href = "Pages/abstimmung_page";
+});
